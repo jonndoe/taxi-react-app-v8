a0bf9aa441fc1c4aa921743af17f38eafc97be4d
jonndoe <user@mail.com>
2021-05-07 09:23:49 +0100

    one test fail, 'can receive a ride request'

--- client/src/App.js
+++ client/src/App.js
@@ -53,6 +53,15 @@ function App () {
         <Navbar.Toggle />
         <Navbar.Collapse>
           {
+            isRider() && (
+              <Nav className='mr-auto'>
+                <LinkContainer to='/rider/request'>
+                  <Nav.Link>Request a trip</Nav.Link>
+                </LinkContainer>
+              </Nav>
+            )
+          }
+          {
             isLoggedIn && (
             <Form inline className='ml-auto'>
               <Button type='button' onClick={ () => logOut() }>Log out</Button>
